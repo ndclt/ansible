b7f91abe932a9095ef14e2152c572021e07dd557
ndclt <nicolas.duclert@estaca.eu>
2019-02-10 22:00:14 +0100

    test: mock the connect request

--- test/units/modules/identity/keycloak/test_keycloak_user.py
+++ test/units/modules/identity/keycloak/test_keycloak_user.py
@@ -1,5 +1,22 @@
+from __future__ import (absolute_import, division, print_function)
+
+import pytest
+
 from ansible.modules.identity.keycloak import keycloak_user
-from units.modules.utils import AnsibleExitJson, AnsibleFailJson, fail_json, exit_json, set_module_args
+from units.modules.utils import (
+    AnsibleExitJson, AnsibleFailJson, fail_json, exit_json, set_module_args)
+from ansible.module_utils.six import BytesIO
+from io import TextIOWrapper
+
+
+@pytest.fixture
+def open_url_mock(mocker):
+    return mocker.patch(
+        'ansible.module_utils.keycloak.open_url',
+        # 'ansible.modules.identity.keycloak.open_url',
+        side_effect=mocked_requests_get,
+        autospec=True
+    )
 
 
 # def get_bin_path(arg, required=False):
@@ -10,17 +27,25 @@ from units.modules.utils import AnsibleExitJson, AnsibleFailJson, fail_json, exi
 #         if required:
 #             fail_json(msg='%r not found !' % arg)
 
+def mocked_requests_get(*args, **kwargs):
+    if args[0] == 'http://keycloak.url/auth/realms/master/protocol/openid-connect/token':
+        return TextIOWrapper(BytesIO(b'{"access_token": "a long token"}'), encoding='utf8')
+    else:
+        pass
 
-def test_nothing_to_do(monkeypatch, capsys):
-    monkeypatch.setattr(keycloak_user.AnsibleModule, 'exit_json', fail_json)
+
+def test_nothing_to_do(monkeypatch, open_url_mock):
+    monkeypatch.setattr(keycloak_user.AnsibleModule, 'exit_json', exit_json)
+    monkeypatch.setattr(keycloak_user.AnsibleModule, 'fail_json', fail_json)
     set_module_args(
         {
-            'auth_keycloak_url': 'http://keycloak.url',
-            'auth_username': 'nd',
-            'auth_password': 'nd',
+            'auth_keycloak_url': 'http://keycloak.url/auth',
+            'auth_username': 'test_admin',
+            'auth_password': 'admin_password',
             'auth_realm': 'master',
-            'keycloak_username': 'blu',
+            'keycloak_username': 'to_not_add_user',
             'state': 'absent'
         })
-    keycloak_user.main()
+    with pytest.raises(AnsibleExitJson) as exec_error:
+        keycloak_user.main()
     assert True
