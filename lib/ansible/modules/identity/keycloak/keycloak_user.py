25cadbf90a9cf263cca5ac32bca523a35e2a332c
Nicolas Duclert <nicolas.duclert@metronlab.com>
2019-02-14 09:36:15 +0100

    doc: change copyright

--- lib/ansible/modules/identity/keycloak/keycloak_user.py
+++ lib/ansible/modules/identity/keycloak/keycloak_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# Copyright: (c) 2018, Nicolas Duclert <nicolas.duclert@metronlab.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
