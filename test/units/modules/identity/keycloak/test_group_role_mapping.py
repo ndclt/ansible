fe6f180db9d94263a849fe45579dbe3452c784ed
Nicolas Duclert <nicolas.duclert@metronlab.com>
2019-04-30 17:00:20 +0200

    test: add usecase with role in client

--- test/units/modules/identity/keycloak/test_group_role_mapping.py
+++ test/units/modules/identity/keycloak/test_group_role_mapping.py
@@ -8,7 +8,13 @@ from units.modules.utils import (
 from ansible.modules.identity.keycloak import keycloak_link_group_role
 
 
-def test_state_absent_without_link_should_not_do_something(monkeypatch):
+@pytest.mark.parametrize('extra_arguments, waited_message', [
+    ({'role_name': 'one_role'},
+     'Links between one_group and one_role does not exist, doing nothing.'),
+    ({'role_name': 'role_in_client', 'client_id': 'one_client'},
+     'Links between one_group and role_in_client in one_client does_not_exist, doing nothing.')
+], ids=['role in realm master', 'role in client'])
+def test_state_absent_without_link_should_not_do_something(monkeypatch, extra_arguments, waited_message):
     monkeypatch.setattr(keycloak_link_group_role.AnsibleModule, 'exit_json', exit_json)
     monkeypatch.setattr(keycloak_link_group_role.AnsibleModule, 'fail_json', fail_json)
     arguments = {
@@ -19,12 +25,11 @@ def test_state_absent_without_link_should_not_do_something(monkeypatch):
         'realm': 'master',
         'state': 'absent',
         'group_name': 'one_group',
-        'role_name': 'one_role'
     }
+    arguments.update(extra_arguments)
 
     set_module_args(arguments)
     with pytest.raises(AnsibleExitJson) as exec_trace:
         keycloak_link_group_role.main()
     ansible_exit_json = exec_trace.value.args[0]
-    assert ansible_exit_json['msg'] == (
-        'Links between one_group and one_role does not exist, doing nothing.')
+    assert ansible_exit_json['msg'] == waited_message
