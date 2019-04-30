d4953fb56da9fe1ed300da4c605c5077ee25079b
Nicolas Duclert <nicolas.duclert@metronlab.com>
2019-04-30 10:38:00 +0200

    refactor: rename file with group instead of user

--- test/units/modules/identity/keycloak/test_group_role_mapping.py
+++ test/units/modules/identity/keycloak/test_group_role_mapping.py
@@ -5,12 +5,12 @@ import pytest
 
 from units.modules.utils import (
     AnsibleExitJson, AnsibleFailJson, fail_json, exit_json, set_module_args)
-from ansible.modules.identity.keycloak import keycloak_link_role_user
+from ansible.modules.identity.keycloak import keycloak_link_group_role
 
 
 def test_state_absent_without_link_should_not_do_something(monkeypatch):
-    monkeypatch.setattr(keycloak_link_role_user.AnsibleModule, 'exit_json', exit_json)
-    monkeypatch.setattr(keycloak_link_role_user.AnsibleModule, 'fail_json', fail_json)
+    monkeypatch.setattr(keycloak_link_group_role.AnsibleModule, 'exit_json', exit_json)
+    monkeypatch.setattr(keycloak_link_group_role.AnsibleModule, 'fail_json', fail_json)
     arguments = {
         'auth_keycloak_url': 'http://keycloak.url/auth',
         'auth_username': 'test_admin',
@@ -24,7 +24,7 @@ def test_state_absent_without_link_should_not_do_something(monkeypatch):
 
     set_module_args(arguments)
     with pytest.raises(AnsibleExitJson) as exec_trace:
-        keycloak_link_role_user.main()
+        keycloak_link_group_role.main()
     ansible_exit_json = exec_trace.value.args[0]
     assert ansible_exit_json['msg'] == (
         'Links between one_user and one_role does not exist, doing nothing.')
