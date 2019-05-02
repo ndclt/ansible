7a22e681d534c66dd573a733c62393bd202aeb04
Nicolas Duclert <nicolas.duclert@metronlab.com>
2019-05-02 08:56:05 +0200

    wip: add test for link creation

--- test/units/modules/identity/keycloak/test_group_role_mapping.py
+++ test/units/modules/identity/keycloak/test_group_role_mapping.py
@@ -33,3 +33,22 @@ def test_state_absent_without_link_should_not_do_something(monkeypatch, extra_ar
         keycloak_link_group_role.main()
     ansible_exit_json = exec_trace.value.args[0]
     assert ansible_exit_json['msg'] == waited_message
+
+
+def test_state_present_without_link_should_create_link(monkeypatch):
+    monkeypatch.setattr(keycloak_link_group_role.AnsibleModule, 'exit_json', exit_json)
+    monkeypatch.setattr(keycloak_link_group_role.AnsibleModule, 'fail_json', fail_json)
+    arguments = {
+        'auth_keycloak_url': 'http://keycloak.url/auth',
+        'auth_username': 'test_admin',
+        'auth_password': 'admin_password',
+        'auth_realm': 'master',
+        'realm': 'master',
+        'state': 'absent',
+        'group_name': 'one_group',
+    }
+    set_module_args(arguments)
+    with pytest.raises(AnsibleExitJson) as exec_trace:
+        keycloak_link_group_role.main()
+    ansible_exit_json = exec_trace.value.args[0]
+    assert ansible_exit_json['msg'] == ()
