b9d7e408258eecf36e68417b46aa155cf3dae981
ndclt <nicolas.duclert@estaca.eu>
2019-05-15 15:43:54 +0200

    doc: add module documentation

--- lib/ansible/modules/identity/keycloak/keycloak_group_role_mapping.py
+++ lib/ansible/modules/identity/keycloak/keycloak_group_role_mapping.py
@@ -1,5 +1,148 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Copyright: (c) 2018, Nicolas Duclert <nicolas.duclert@metronlab.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = r'''
+---
+module keycloak_group_role_mapping
+
+short_description: Allows adminstration of Keycloak mapping between group and role via Keycloak API
+
+version_added: "2.9"
+
+description:
+    - This module allows the administration of Keycloak clients via the Keycloak REST API. It
+      requires access to the REST API via OpenID Connect; the user connecting and the client being
+      used must have the requisite access rights. In a default Keycloak installation, admin-cli
+      and an admin user would work, as would a separate client definition with the scope tailored
+      to your needs and a user having the expected roles.
+
+    - The names of module options are snake_cased versions of the camelCase ones found in the
+      Keycloak API and its documentation at U(https://www.keycloak.org/docs-api/4.8/rest-api/index.html/).
+      Aliases are provided so camelCased versions can be used as well. If they are in conflict
+      with ansible names or previous used names, they will be prefixed by "keycloak".
+    
+    - The group, role and client should exist before the call to this module. If not,
+      a error message will be return.
+
+options:
+    state:
+        description:
+            - State of the mapping
+            - On C(present), the mapping between role and group will be created if it not exists.
+            - On C(absent), the mapping between role and group will be removed if it exists
+        type: str
+        choices: [ present, absent ]
+        default: present
+
+    realm:
+        description:
+            - The realm where the role, group and optionaly client are.
+        type: str
+        default: master
+    
+    group_name:
+        description:
+            - Name of the group
+            - This parameter is mutually exclusive with group_id and one of 
+            them is required by the module.
+        aliases: [ groupName ]
+        type: str
+    
+    group_id:
+        description:
+            - Id (as a uuid) of the group
+            - This parameter is mutually exclusive with group_name and one of 
+            them is required by the module.
+        aliases: [ groupId ]
+        type: str
+    
+    role_name:
+        description:
+            - Name of the role
+            - This parameter is mutually exclusive with role_id and one of 
+            them is required by the module.
+        aliases: [ roleName ]
+        type: str
+    
+    role_id:
+        description:
+            - Id (as a uuid) of the role
+            - This parameter is mutually exclusive with role_name and one of 
+            them is required by the module.
+        aliases: [ roleId ]
+        type: str
+
+    client_id:
+        description:
+            - Client id of client where the given role will be search. This is
+            usually an alphanumeric name chosen by you.
+        type: str
+        aliases: [ clientId ]
+
+extends_documentation_fragment:
+    - keycloak
+
+author:
+    - Nicolas Duclert (@ndclt) <nicolas.duclert@metronlab.com>
+'''
+
+EXAMPLES = r'''
+- name: create the mapping if it does not exist
+  keycloak_group_role_mapping:
+    auth_client_id: admin-cli
+    auth_keycloak_url: http://auth.example.com/auth
+    auth_realm: master
+    auth_username: USERNAME
+    auth_password: PASSWORD
+    realm: master
+    group_name: one_group
+    role_name: one_role
+- name delete the mapping if it exists
+  keycloak_group_role_mapping:
+    auth_client_id: admin-cli
+    auth_keycloak_url: http://auth.example.com/auth
+    auth_realm: master
+    auth_username: USERNAME
+    auth_password: PASSWORD
+    realm: master
+    state: absent
+    group_name: one_group
+    role_name: one_role
+'''
+
+RETURN = r'''
+msg:
+  description: Message as to what action was taken
+  returned: always
+  type: str
+  sample: "Link between one_group and one_role created."
+  
+changed:
+  description: whether the state of the keycloak configuration change
+  returned: always
+  type: bool
+
+roles_in_group:
+  description: the mapped role if it exists.
+  returned: always
+  type: dict
+  sample: {
+    'name': 'already_link_role',
+    'id': 'ffffffff-1111-1111-1111-111111111111',
+    'description': 'showing why this role exists',
+  }
+'''
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -13,11 +156,11 @@ def run_module():
         state=dict(type='str', default='present',
                    choices=['present', 'absent']),
         realm=dict(type='str', default='master'),
-        group_name=dict(type='str', required=False),
-        group_id=dict(type='str', required=False),
-        client_id=dict(type='str', aliases=['clientId'], required=False),
+        group_name=dict(type='str', aliases=['groupName']),
+        group_id=dict(type='str', aliases=['groupId']),
         role_name=dict(type='str', aliases=['roleName']),
         role_id=dict(type='str', aliases=['roleId']),
+        client_id=dict(type='str', aliases=['clientId'], required=False),
     )
 
     argument_spec.update(meta_args)
